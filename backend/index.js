8702ed3139c24a77b76099c48cd9a4616d95b9ee
Øystein Holvik Johnsen <oystein@holvikjohnsen.no>
2021-11-25 22:51:20 +0100

    Trigger rebuild

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Wooooorld!");
 });
 
 app.listen(port, () => {
